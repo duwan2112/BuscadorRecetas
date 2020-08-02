273365903aa7deb78d62241edbad4b0ad17e1889
Duwan Peña <duwanp2112@gmail.com>
2020-08-02 11:56:46 -0430

    Updates

---
+++ precache-manifest.4b54ac2a1a2cfd6b06ab03600df788fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71b8609b1a345fe7768d0d4cc0a7f352",
+    "url": "/BuscadorRecetas/index.html"
+  },
+  {
+    "revision": "10cc416161d434a8959f",
+    "url": "/BuscadorRecetas/static/css/main.b20bf0df.chunk.css"
+  },
+  {
+    "revision": "ad25c25c4959c3d28154",
+    "url": "/BuscadorRecetas/static/js/2.785077bf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/BuscadorRecetas/static/js/2.785077bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10cc416161d434a8959f",
+    "url": "/BuscadorRecetas/static/js/main.a9d05c1e.chunk.js"
+  },
+  {
+    "revision": "e1e87e8aad3fc04fbb45",
+    "url": "/BuscadorRecetas/static/js/runtime-main.9cc6206a.js"
+  }
+]);
